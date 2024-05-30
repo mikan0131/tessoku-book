2093417450bcc69c9d1887642b9b536e59cb3e15
mikan0131 <kotaro9111@gmail.com>
2024-05-30 17:50:10 +0900

    B18.cpp add

---
+++ B18.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, S, A[69];
+bool dp[69][10009];
+
+int main() {
+    cin >> N >> S;
+    for (int i = 1; i <= N; i++) cin >> A[i];
+    
+}
