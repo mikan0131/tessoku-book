1547a690f139350393d98270a4605ff946c22922
mikan0131 <kotaro9111@gmail.com>
2024-06-08 17:33:24 +0900

    A55 change

--- A55.cpp
+++ A55.cpp
@@ -2,23 +2,19 @@
 #include <set>
 using namespace std;
 
-int Q;
-set <int> X;
+int Q, T[100009], x[100009];
 
 int main() {
     cin >> Q;
+    for (int i = 1; i <= Q; i++) cin >> T[i] >> x[i];
+    set <int> S;
     for (int i = 1; i <= Q; i++) {
-        int T;
-        cin >> T;
-        int x;
-        cin >> x;
-        if (T == 1) X.insert(x);
-        if (T == 2) {
-            if (X.count(x) == true) X.erase(x);
-            else cout << -1 << endl;
-        }
-        if (T == 3) {
-            if (X.count(x) == true) 
+        if (T[i] == 1) S.insert(x[i]);
+        if (T[i] == 2) S.erase(x[i]);
+        if (T[i] == 3) {
+            auto itr = S.lower_bound(x[i]);
+            if (itr == S.end()) cout << -1 << endl;
+            else cout << (*itr) << endl;
         }
     }
     return 0;
