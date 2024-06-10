7d765427a3aefee65a9e5f74bf1f41e0e748c4e0
mikan0131 <kotaro9111@gmail.com>
2024-06-10 16:16:49 +0900

    B13 add

--- B13.cpp
+++ B13.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 long long N, K;
-long long A[100009], S[100009], R[100009];
+long long A[100009];
+long long S[100009];
+long long R[100009];
 
-long long sum (int l, int r) {
+long long sum(int l, int r) {
     return S[r] - S[l - 1];
 }
 
@@ -14,16 +16,14 @@ int main() {
     S[0] = 0;
     for (int i = 1; i <= N; i++) S[i] = S[i - 1] + A[i];
     for (int i = 1; i <= N; i++) {
-        if (i == 1) R[i] = 0;
+        if(i == 1) R[i] = 0;
         else R[i] = R[i - 1];
         while (R[i] < N && sum(i, R[i] + 1) <= K) {
             R[i]++;
         }
-        
     }
     long long Answer = 0;
-    for (int i = 1; i <= N; i++) Answer += (R[i] - i + 1);
-        
+    for (int i = 1; i <= N; i++) Answer += R[i] - i + 1;
     cout << Answer << endl;
     return 0;
 }
